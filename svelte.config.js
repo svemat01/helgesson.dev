21f908079af08ed40555cf3a88447da87be0c979
Svemat01 <jakob.helgesson@gmail.com>
2022-08-09 09:01:21 +0200

    Use my sveltekit template

---
+++ svelte.config.js
@@ -0,0 +1,24 @@
+import adapter from '@sveltejs/adapter-static';
+import preprocess from 'svelte-preprocess';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+    // Consult https://github.com/sveltejs/svelte-preprocess
+    // for more information about preprocessors
+    preprocess: [
+        preprocess({
+            scss: {
+                prependData: '@use "src/lib/styles/variables.scss" as *;',
+            },
+        }),
+    ],
+
+    kit: {
+        adapter: adapter(),
+        preprocess: {
+            default: true,
+        },
+    },
+};
+
+export default config;
---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import { sveltekit } from '@sveltejs/kit/vite';
+
+/** @type {import('vite').UserConfig} */
+const config = {
+    plugins: [sveltekit()],
+
+    css: {
+        preprocessorOptions: {
+            scss: {
+                additionalData: '@use "src/lib/styles/variables.scss" as *;',
+            },
+        },
+    },
+};
+
+export default config;

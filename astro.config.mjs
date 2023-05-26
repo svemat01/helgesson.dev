0fcf568de85ac9e949c895276c212010573896b4
Svemat <jakob.helgesson@gmail.com>
2023-05-26 13:15:25 +0000

    Rework into astro project

---
+++ astro.config.mjs
@@ -0,0 +1,17 @@
+import { defineConfig } from "astro/config";
+
+import mdx from "@astrojs/mdx";
+
+// https://astro.build/config
+export default defineConfig({
+    vite: {
+        css: {
+            preprocessorOptions: {
+                scss: {
+                    additionalData: '@use "src/styles/variables.scss" as *;',
+                },
+            },
+        },
+    },
+    integrations: [mdx()],
+});
